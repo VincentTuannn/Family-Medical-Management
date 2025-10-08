c63e6ca8ac246624fae1a160cec95a68cdd502b3
VincentTuannn <vincenttuan1098@gmail.com>
2025-10-08 10:06:39 +0700

    initiate project

---
+++ Family-Medical-Management/src/main/java/J2EE/Family/Medical/Management/FamilyMedicalManagementApplication.java
@@ -0,0 +1,13 @@
+package J2EE.Family.Medical.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyMedicalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyMedicalManagementApplication.class, args);
+	}
+
+}
---
+++ Family-Medical-Management/src/test/java/J2EE/Family/Medical/Management/FamilyMedicalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package J2EE.Family.Medical.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyMedicalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
